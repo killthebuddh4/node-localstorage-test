54aab7d509571654855ac702b298ded721d4fe2d
achilles schmelzer <achilles@daopanel.com>
2022-07-22 10:49:53 -0700

    Explore serverless runtime filesystem

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Client } from "@xmtp/xmtp-js";
+// import { Client } from "@xmtp/xmtp-js";
+import * as fs from 'fs';
 
 export default function handler(req, res) {
+fs.readdirSync('.').forEach(file => {
+  console.log(file);
+});
   res.status(200).json({ name: 'test' })
 }
