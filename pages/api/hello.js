a28466e05925a2fbadcfed4be7c18289ea59bd68
achilles schmelzer <achilles@daopanel.com>
2022-07-22 11:09:55 -0700

    Test line from node-localstorage

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // import { Client } from "@xmtp/xmtp-js";
 import * as fs from 'fs';
+import path from 'path';
 
 export default function handler(req, res) {
-fs.readdirSync('/var').forEach(file => {
-  console.log(file);
-});
+  fs.readdirSync('.').forEach(file => {
+    console.log(file);
+  });
+  fs.mkdirSync(path.resolve('./scratch'), { recursive: true })
+  
   res.status(200).json({ name: 'test' })
 }
