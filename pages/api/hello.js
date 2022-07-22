3074e1a7694d9bdb6f55fffcc32d81ee3a35a6e0
achilles schmelzer <achilles@daopanel.com>
2022-07-22 10:23:28 -0700

    Add xmtp to API route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Client } from "@xmtp/xmtp-js";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: String(Client) })
 }
