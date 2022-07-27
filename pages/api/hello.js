10e1707a66c6806ed74425aef8ebb076b5cd1623
achilles schmelzer <achilles@daopanel.com>
2022-07-27 16:58:36 -0700

    test 1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,18 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // import { Client } from "@xmtp/xmtp-js";
-import * as fs from 'fs';
-import path from 'path';
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  global.localStorage = new LocalStorage('./scratch');
+}
 
 export default function handler(req, res) {
-  // fs.readdirSync('.').forEach(file => {
-  //   console.log(file);
-  // });
-  // const p = path.resolve('./scratch');
-  // console.log(p);
-  // console.log(fs.statSync('/var'))
-  // console.log(fs.statSync('/var/task'))
-  fs.mkdirSync('/tmp/scratch');
-  // console.log(fs.statSync('/var/task/scratch'))
-  
   res.status(200).json({ name: 'test' })
 }
