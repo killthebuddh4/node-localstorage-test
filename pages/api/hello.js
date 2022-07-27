2783fdddc3ffa562b972b942a2ef029c0cea6fba
achilles schmelzer <achilles@daopanel.com>
2022-07-27 14:28:34 -0700

    Another test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,14 +4,14 @@ import * as fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  fs.readdirSync('.').forEach(file => {
-    console.log(file);
-  });
-  const p = path.resolve('./scratch');
-  console.log(p);
-  console.log(fs.statSync('/var'))
-  console.log(fs.statSync('/var/task'))
-  fs.mkdirSync('/var/task/scratch');
+  // fs.readdirSync('.').forEach(file => {
+  //   console.log(file);
+  // });
+  // const p = path.resolve('./scratch');
+  // console.log(p);
+  // console.log(fs.statSync('/var'))
+  // console.log(fs.statSync('/var/task'))
+  fs.mkdirSync('/tmp/scratch');
   // console.log(fs.statSync('/var/task/scratch'))
   
   res.status(200).json({ name: 'test' })
